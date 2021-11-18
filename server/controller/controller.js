ee5ac06aeabd9b4207db16ecbee0b0b760a4e977
MAHADEVAN-A <68148704+MAHADEVAN-A@users.noreply.github.com>
2021-11-18 07:22:45 +0530

    Add files via upload

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,9 +1,44 @@
 const fs = require('fs');
-const UploadModel = require('../model/schema');
+const Product = require('../model/productSchema');
+const Company = require('../model/companySchema');
 
 exports.home = async (req, res) => {
-  const all_images = await UploadModel.find();
+  const all_images = await Product.find();
   res.render('main', { images: all_images });
+};
+
+exports.createCompany = async (req, res) => {
+  try {
+    const data = req.body;
+    const comp = await Company.create(data);
+    res.send(comp);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.fetchCompany = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const comp = await Company.findOne({ email: email });
+    res.status(200).json({ comp });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.getImage = async (req, res) => {
+  const all_images = await Product.find();
+  res.json(all_images);
+};
+
+exports.deleteImage = async (req, res) => {
+  const imgID = req.params.id;
+  const image = await Product.findOneAndDelete({ _id: imgID });
+  if (!image) {
+    return res.status(404).json({ msg: `No task with id:${imgID}` });
+  }
+  res.status(200).json({ image });
 };
 
 exports.uploads = (req, res, next) => {
@@ -23,6 +58,10 @@ exports.uploads = (req, res, next) => {
   let result = imgArray.map((src, index) => {
     //create object to store data in collection
     let finalimg = {
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      company: req.body.company,
       filename: files[index].originalname,
       contentType: files[index].mimetype,
       imageBase64: src,
